cafd32e933404c1552338bce935e3d25ed90e5d5
Leonardo Fabian Hernandez Peña <leonardohernandez.fp@academia.umb.edu.co>
2018-05-20 22:56:23 -0500

    se hace la prueba de vista.

---
+++ resources/views/LoginView.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Esta es la pagina de Login">
+    <meta name="author" content="Alexander y Leonardo">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <title>Login Adminisrativo</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="wrapper">
+      <div>
+        <ul>
+          <li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
