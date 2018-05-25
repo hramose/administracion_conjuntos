dc9f535262aee46fd413edfff055c3fc4f56a2a3
Leonardo Fabian Hernandez Peña <leonardohernandez.fp@academia.umb.edu.co>
2018-05-25 00:58:44 -0500

    Finalización del welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -108,11 +108,11 @@
                 </div>
                 <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
+                  <span class="sr-only">Anterior</span>
                 </a>
                 <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
+                  <span class="sr-only">Siguiente</span>
                 </a>
               </div>
             </header>
