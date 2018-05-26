391444b56ee816cb86a3a1149c3d44272d3f8666
Alex <alexnarfo5@gmail.com>
2018-05-26 13:35:43 -0500

    cambios de vista para index se agrega carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,119 +4,75 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-        <!-- Styles -->
-        <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>Administracion Conjuntos</title>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <header>
-              <div id="carousel" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                  <li data-target="#carousel" data-slide-to="1"></li>
-                  <li data-target="#carousel" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner" role="listbox">
-                  <!-- En esta pagina se creará el index en forma de carrusel -->
-                  <div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
-                    <div class="carousel-caption d-none d-md-block">
-                      <h3>Prueba uno</h3>
-                      <p>se crea una Prueba</p>
-                    </div>
-                  </div>
-                  <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
-                    <div class="carousel-caption d-none d-md-block">
-                      <h3>Prueba dos</h3>
-                      <p>se crea la prueba.</p>
-                    </div>
-                  </div>
-                  <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
-                    <div class="carousel-caption d-none d-md-block">
-                      <h3>Prueba tres</h3>
-                      <p>ejemplo de prueba tres.</p>
-                    </div>
-                  </div>
-                </div>
-                <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Siguiente</span>
-                </a>
+        <header>
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <a class="navbar-brand" href="#">Administracion Conjuntos</a>
               </div>
-            </header>
-
-        </div>
+              @if (Route::has('login'))
+                @auth
+                  <div class="navbar-header">
+                    <a href="{{ url('/home') }}">Home</a>
+                  </div>
+                @else
+                    <ul class="nav navbar-nav navbar-right">
+                      <li><a href="{{ route('login') }}">Iniciar Sesión</a></li>
+                      <li><a href="{{ route('register') }}">Registrase</a></li>
+                    </ul>
+                @endauth
+              @endif
+            </div>
+          </nav>
+        </header>
+        <article>
+          <div class="full-container">
+            
+            <div id="jrCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#jrCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#jrCarousel" data-slide-to="1"></li>
+                  <li data-target="#jrCarousel" data-slide-to="2"></li>
+                </ol>
+              
+                <!-- Wrapper for slides --> 
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <img src="{{ asset('img/administracion_1.jpg') }}" class="img-carousel img-responsive" alt="admin-5">
+                  </div>
+              
+                  <div class="item">
+                    <img src="{{ asset('img/administracion_2.jpg') }}" class="img-carousel img-responsive" alt="admin-6">
+                  </div>
+              
+                  <div class="item">
+                    <img src="{{ asset('img/administracion_3.jpg') }}" class="img-carousel img-responsive" alt="admin-1">
+                  </div>
+                </div>
+              
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#jrCarousel" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#jrCarousel" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+            </div>
+          </div>
+        </article>
     </body>
 </html>
