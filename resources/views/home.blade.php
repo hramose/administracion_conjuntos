0c14fd6f870ae01885e44c34c9a91485ec3b0b68
Leonardo Fabian Hernandez Peña <leonardohernandez.fp@academia.umb.edu.co>
2018-05-27 13:53:42 -0500

    Cambio de imagenes en home.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
         @endif
         <div class="col-md-4">
           <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="{{ asset('img/eje1.jpg') }}" alt="Card image cap">
+              <img class="card-img-top" src="{{ asset('img/eje4.jpg') }}" alt="Card image cap">
               <div class="card-body">
                   <h5 class="card-title">Administrar facturas</h5>
                   <p class="card-text">En esta sección podrás generar y administrar las facturas de cambio.</p>
@@ -20,7 +20,7 @@
         </div>
         <div class="col-md-4">
           <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="{{ asset('img/eje2.jpg') }}" alt="Card image cap">
+              <img class="card-img-top" src="{{ asset('img/eje5.jpg') }}" alt="Card image cap">
               <div class="card-body">
                   <h5 class="card-title">Perfil</h5>
                   <p class="card-text">Para editar mi información básica y contraseña</p>
@@ -30,7 +30,7 @@
         </div>
         <div class="col-md-4">
           <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="{{ asset('img/eje4.jpg') }}" alt="Card image cap">
+              <img class="card-img-top" src="{{ asset('img/eje6.jpg') }}" alt="Card image cap">
               <div class="card-body">
                   <h5 class="card-title">Estado de Cuenta</h5>
                   <p class="card-text">Aquí podras revisar los últimos pagos realizados y mi resumen de mi estado de cuenta actual.</p>
