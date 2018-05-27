0eeb8956dcb443def242ac999de1021a8424dd97
Leonardo Fabian Hernandez Peña <leonardohernandez.fp@academia.umb.edu.co>
2018-05-27 13:44:34 -0500

    Se corrigen la información de las cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,9 +12,9 @@
           <div class="card" style="width: 18rem;">
               <img class="card-img-top" src="{{ asset('img/eje1.jpg') }}" alt="Card image cap">
               <div class="card-body">
-                  <h5 class="card-title">Administrador</h5>
-                  <p class="card-text">En esta sección está dedicada para el Administrador.</p>
-                  <a href="#" class="btn btn-primary">ir a administración</a>
+                  <h5 class="card-title">Administrar facturas</h5>
+                  <p class="card-text">En esta sección podrás generar y administrar las facturas de cambio.</p>
+                  <a href="#" class="btn btn-primary">ir a administrar</a>
               </div>
           </div>
         </div>
@@ -23,8 +23,8 @@
               <img class="card-img-top" src="{{ asset('img/eje2.jpg') }}" alt="Card image cap">
               <div class="card-body">
                   <h5 class="card-title">Perfil</h5>
-                  <p class="card-text">Aquí podrás entrar en las siguientes caracteristicas que sólo los propietarios podrán ver</p>
-                  <a href="#" class="btn btn-primary">Ir a mi propiedad</a>
+                  <p class="card-text">Para editar mi información básica y contraseña</p>
+                  <a href="#" class="btn btn-primary">Ir a mi perfil</a>
               </div>
           </div>
         </div>
@@ -33,7 +33,7 @@
               <img class="card-img-top" src="{{ asset('img/eje4.jpg') }}" alt="Card image cap">
               <div class="card-body">
                   <h5 class="card-title">Estado de Cuenta</h5>
-                  <p class="card-text">Aquí podras revisar los últimos pagos realizado y generar los pagos realizados.</p>
+                  <p class="card-text">Aquí podras revisar los últimos pagos realizados y mi resumen de mi estado de cuenta actual.</p>
                   <a href="#" class="btn btn-primary">Ir a mis estado de cuenta</a>
               </div>
           </div>
